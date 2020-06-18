18c06b1cbe96aed45d23f1b3fc3c712fd70cd1eb
Arturo Chen <art.can.fly@gmail.com>
2020-06-18 17:12:09 -0400

    E2.4 tokens appear within container

--- workshop/exercise-2.4/app.js
+++ workshop/exercise-2.4/app.js
@@ -3,33 +3,38 @@ const CLICKED_COLOR = 'darkgreen';
 const FONT_COLOR = 'white';
 const FONT_SIZE = '30px';
 const FONT_WEIGHT = '700';
-const token_SIDE_LENGTH = '100px';
+const token_SIDE_LENGTH = '100';
 
 const game = document.querySelector('.game');
+const gameSection = document.querySelector('.game-section');
+const offsetHeight = game.offsetHeight - token_SIDE_LENGTH;
+const offsetWidth = game.offsetWidth - token_SIDE_LENGTH;
 
 for(let i = 1; i <= 20; i++) {
     const token = document.createElement('div');
-    token.style.width = token_SIDE_LENGTH;
-    token.style.height = token_SIDE_LENGTH;
+    token.style.width = `${token_SIDE_LENGTH}px`;
+    token.style.height = `${token_SIDE_LENGTH}px`;
     token.style.backgroundColor = UNCLICKED_COLOR;
     token.style.color = FONT_COLOR;
     token.style.fontSize = FONT_SIZE;
     token.style.fontWeight = FONT_WEIGHT;
     token.style.borderRadius = '50%';
-    token.style.position = 'relative';
-    token.style.top = `${Math.floor(Math.random() * 300)}px`;
-    token.style.left = `${Math.floor(Math.random() * 300)}px`;
+    token.style.position = 'absolute';
+    
+    token.style.top = `${Math.floor(Math.random() * offsetHeight)}px`;
+    token.style.left = `${Math.floor(Math.random() * offsetWidth)}px`;
     token.style.display = 'flex';
     token.style.justifyContent = 'center';
     token.style.alignItems = 'center';
     token.innerHTML = i;
-
+    
     token.addEventListener('click', () => {
         let currentColor = token.style.backgroundColor;
         token.style.backgroundColor = (currentColor == CLICKED_COLOR) ? UNCLICKED_COLOR : CLICKED_COLOR;
     });
-
-    // game.appendChild(token);
+    
+    game.appendChild(token);
 }
 
+console.log();
 document.querySelector('.game-section').appendChild(game);
