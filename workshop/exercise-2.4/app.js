e9571eec97ed2dc873b7c83af42facf2f371af53
Arturo Chen <art.can.fly@gmail.com>
2020-06-18 19:23:17 -0400

    E2.4 Completed

--- workshop/exercise-2.4/app.js
+++ workshop/exercise-2.4/app.js
@@ -4,14 +4,20 @@ const FONT_COLOR = 'white';
 const FONT_SIZE = '30px';
 const FONT_WEIGHT = '700';
 const token_SIDE_LENGTH = '100';
+const TOKENS = Math.floor(Math.random() * 10) + 1;
+
+let won = false;
 let TIME = 5;
+let clicked = 0;
+let endgameMsg = document.createElement('span');
 
 const game = document.querySelector('.game');
 const gameSection = document.querySelector('.game-section');
 const offsetHeight = game.offsetHeight - token_SIDE_LENGTH;
 const offsetWidth = game.offsetWidth - token_SIDE_LENGTH;
 
-for(let i = 1; i <= 20; i++) {
+
+for(let i = 1; i <= TOKENS; i++) {
     const token = document.createElement('div');
     token.style.width = `${token_SIDE_LENGTH}px`;
     token.style.height = `${token_SIDE_LENGTH}px`;
@@ -30,6 +36,9 @@ for(let i = 1; i <= 20; i++) {
     token.innerHTML = i;
     
     token.addEventListener('click', () => {
+        clicked++;
+        totalClicked.innerHTML = `Total Clicked: ${clicked}`;
+        totalUnclicked.innerHTML = `Remaining: ${TOKENS - clicked}`;
         let currentColor = token.style.backgroundColor;
         token.style.backgroundColor = (currentColor == CLICKED_COLOR) ? UNCLICKED_COLOR : CLICKED_COLOR;
     });
@@ -59,9 +68,11 @@ let countdown = document.querySelector('.countdown');
 let timer;
 let updateCountdown = () => {
     if(TIME == 0) {
+        won = clicked === TOKENS;
         countdown.style.color = 'rgb(255, 230, 0)';
         clearInterval(timer)
-        // alert('GAME OVER');
+        endgameMsg.innerHTML = won ? 'You Win!!!!' : 'You Lose...';
+        endgameMsg.style.visibility = 'visible';
     }
     countdown.innerHTML = TIME--;
 }
@@ -71,3 +82,28 @@ let startCountdown = () => {
 }
 
 // Show end of game message.
+endgameMsg.style.color = 'white';
+endgameMsg.style.fontSize = '5rem';
+endgameMsg.style.width = '500px';
+endgameMsg.style.height = '200px';
+endgameMsg.style.display = 'flex';
+endgameMsg.style.justifyContent = 'center';
+endgameMsg.style.alignItems = 'center';
+endgameMsg.style.borderRadius = '10px';
+endgameMsg.style.backgroundColor = won ? UNCLICKED_COLOR : CLICKED_COLOR;
+endgameMsg.style.zIndex = 1;
+endgameMsg.style.position = 'absolute';
+endgameMsg.style.top = `${(game.offsetHeight / 2) - 100}px`;
+endgameMsg.style.left = `${(game.offsetWidth / 2) - 250}px`;
+endgameMsg.style.visibility = 'hidden';
+
+game.appendChild(endgameMsg);
+
+// Status of the game.
+let totalTokens = document.querySelector('#total-tokens');
+let totalClicked = document.querySelector('#total-clicked');
+let totalUnclicked = document.querySelector('#total-unclicked');
+
+totalTokens.innerHTML = `Total Tokens: ${TOKENS}`;
+totalClicked.innerHTML = `Total Clicked: ${clicked}`;
+totalUnclicked.innerHTML = `Remaining: ${TOKENS - clicked}`;
