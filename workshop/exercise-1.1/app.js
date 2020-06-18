a15ff5ff8e52812cfba2d9d1907713a9be6b7c75
Arturo Chen <art.can.fly@gmail.com>
2020-06-18 11:11:52 -0400

    E1.1 done

--- workshop/exercise-1.1/app.js
+++ workshop/exercise-1.1/app.js
@@ -1,3 +1,11 @@
 // Exercise 1.1
 // ------------
-console.log('exercise 1.1');
+let clicked = false;
+
+document.addEventListener('click', () => clicked = true);
+
+let timer = setTimeout(() => {
+    document.querySelector('.result').innerHTML = clicked ? 
+    'You Win!' : 'You Lost!';
+    clearTimeout(timer);
+}, 1000);
