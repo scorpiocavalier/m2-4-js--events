3d6422e8976042b9a2943aaae75b12102d57ff12
Arturo Chen <art.can.fly@gmail.com>
2020-06-18 21:07:38 -0400

    E2.4 disabled buttons in end game.

--- workshop/exercise-2.4/app.js
+++ workshop/exercise-2.4/app.js
@@ -9,6 +9,7 @@ const TOKENS = Math.floor(Math.random() * 10) + 1;
 let won = false;
 let TIME = 5;
 let clicked = 0;
+let started = false;
 let endgameMsg = document.createElement('span');
 
 const game = document.querySelector('.game');
@@ -16,15 +17,16 @@ const gameSection = document.querySelector('.game-section');
 const offsetHeight = game.offsetHeight - token_SIDE_LENGTH;
 const offsetWidth = game.offsetWidth - token_SIDE_LENGTH;
 
-
 for(let i = 1; i <= TOKENS; i++) {
-    const token = document.createElement('div');
+    const token = document.createElement('button');
     token.style.width = `${token_SIDE_LENGTH}px`;
     token.style.height = `${token_SIDE_LENGTH}px`;
     token.style.backgroundColor = UNCLICKED_COLOR;
     token.style.color = FONT_COLOR;
     token.style.fontSize = FONT_SIZE;
     token.style.fontWeight = FONT_WEIGHT;
+    token.style.border = 'none';
+    token.style.outline = 'none';
     token.style.borderRadius = '50%';
     token.style.position = 'absolute';
     
@@ -35,18 +37,49 @@ for(let i = 1; i <= TOKENS; i++) {
     token.style.alignItems = 'center';
     token.innerHTML = i;
     
-    token.addEventListener('click', () => {
-        clicked++;
-        totalClicked.innerHTML = `Total Clicked: ${clicked}`;
-        totalUnclicked.innerHTML = `Remaining: ${TOKENS - clicked}`;
-        let currentColor = token.style.backgroundColor;
-        token.style.backgroundColor = (currentColor == CLICKED_COLOR) ? UNCLICKED_COLOR : CLICKED_COLOR;
-    });
-    
     game.appendChild(token);
 }
 
 document.querySelector('.game-section').appendChild(game);
+
+let stopGame = () => {
+    clearInterval(timer);
+    removeTokenListeners();
+    stopCountdown();
+    showWinner();
+}
+
+// Add event listener to all tokens except endgameMsg.
+let addTokenListeners = () => {
+    for(let i=0; i<game.children.length-1; i++) {
+        let token = game.children[i];
+        token.addEventListener('click', () => handleToken(token));
+    };
+}
+
+// Remove event listener to all tokens except endgameMsg.
+let removeTokenListeners = () => {
+    for(let i=0; i<game.children.length-1; i++) {
+        let token = game.children[i];
+        token.disabled = true;
+        token.removeEventListener('click', () => handleToken(token));
+    };
+}
+
+let handleToken = token => {
+
+    token.disabled = true;
+
+    clicked++;
+    totalClicked.innerHTML = `Total Clicked: ${clicked}`;
+    totalUnclicked.innerHTML = `Remaining: ${TOKENS - clicked}`;
+    
+    let currentColor = token.style.backgroundColor;
+    token.style.backgroundColor = (currentColor == CLICKED_COLOR) ? UNCLICKED_COLOR : CLICKED_COLOR;
+    
+    // Stop game if all tokens are clicked.
+    (TOKENS == clicked) && stopGame();
+}
 
 // Add event handler for start button.
 let start_btn = document.querySelector('.start');
@@ -56,6 +89,9 @@ let startGame = () => {
     start_btn.style.width = '100%';
     start_btn.style.height = '100%';
     start_btn.style.visibility = 'hidden';
+
+    // Add event listener to all tokens except endgameMsg.
+    addTokenListeners();
 
     // Start countdown.
     startCountdown();
@@ -68,12 +104,8 @@ let countdown = document.querySelector('.countdown');
 let timer;
 let updateCountdown = () => {
     if(TIME == 0) {
-        won = clicked === TOKENS;
         countdown.style.color = 'rgb(255, 230, 0)';
-        clearInterval(timer)
-        endgameMsg.innerHTML = won ? 'You Win!!!!' : 'You Lose...';
-        endgameMsg.style.backgroundColor = won ? CLICKED_COLOR : UNCLICKED_COLOR;
-        endgameMsg.style.visibility = 'visible';
+        stopGame();
     }
     countdown.innerHTML = TIME--;
 }
@@ -82,7 +114,10 @@ let startCountdown = () => {
     timer = setInterval(updateCountdown, 1000);
 }
 
+let stopCountdown = () => countdown.style.animationPlayState = 'paused';
+
 // Show end of game message.
+endgameMsg.style.boxShadow = '8px 8px 6px 2px #888';
 endgameMsg.style.color = 'white';
 endgameMsg.style.fontSize = '5rem';
 endgameMsg.style.width = '500px';
@@ -98,6 +133,13 @@ endgameMsg.style.left = `${(game.offsetWidth / 2) - 250}px`;
 endgameMsg.style.visibility = 'hidden';
 
 game.appendChild(endgameMsg);
+
+const showWinner = () => {
+    won = clicked === TOKENS;
+    endgameMsg.innerHTML = won ? 'You Win!!!!' : 'You Lose...';
+    endgameMsg.style.backgroundColor = won ? CLICKED_COLOR : UNCLICKED_COLOR;
+    endgameMsg.style.visibility = 'visible';
+}
 
 // Status of the game.
 let totalTokens = document.querySelector('#total-tokens');
