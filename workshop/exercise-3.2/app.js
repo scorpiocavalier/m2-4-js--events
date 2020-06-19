7377f5f176201b386814cf79568c5058b6ece18e
Arturo Chen <art.can.fly@gmail.com>
2020-06-18 22:56:06 -0400

    E3.2 stopwatch starts

---
+++ workshop/exercise-3.2/app.js
@@ -0,0 +1,43 @@
+let paused = true;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+let stopwatch = document.querySelector('#stopwatch');
+
+let button = document.querySelector('#btn');
+
+let updateWatch = () => stopwatch.innerHTML = `${hours}:${minutes}:${seconds}`;
+
+let start = () => setInterval(startCount, 1000);
+
+let stop = () => clearInterval(start);
+
+let handleClick = () => {
+    console.log('1. paused: ', paused);
+    paused ? start() : stop();
+    paused = !paused;
+    console.log('2. paused: ', paused);
+}
+
+let startCount = () => {
+    seconds++;
+    if(seconds == 60) {
+        minutes++;
+        seconds = 0;
+        if(minutes == 60) {
+            hours++;
+            minutes = 0;
+        }
+    }
+    updateWatch();
+}
+
+let app = () => {
+    updateWatch();
+    
+    button.addEventListener('click', handleClick);
+    button.innerHTML = 'START / STOP';
+}
+
+app();
